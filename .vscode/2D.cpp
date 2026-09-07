b207f2270a466bbd6a9fc6a6a990021fe4bfa8af
Mithleshk595 <mithleshk0982@gmail.com>
2026-09-07 18:58:58 +0530

    Added 2D.cpp

---
+++ .vscode/2D.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int arr[2][3];
+	arr[0][0] = 6;
+	arr[0][1] = 1;
+	arr[0][2] = 2;
+	arr[1][0] = 4;
+	arr[1][1] = 3;
+	arr[1][2] = 5;
+	for(int j=0; j<3;j++){ // rows
+		for(int i=0;j<2;j++){ // colomnm
+		
+			cout<<arr[i][j]<<" ";
+			
+		}
+		cout<<endl;
+		
+	
+	}
