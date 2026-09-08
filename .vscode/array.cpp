df20acc60d5a577797bed2b9f1c8b81aa9f12958
Mithleshk595 <mithleshk0982@gmail.com>
2026-09-08 21:14:04 +0530

    Added array.cpp

---
+++ .vscode/array.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+    cout<<"Enter n : ";
+	cin>>n;
+	// 1 3 5 7 9
+	// 4 7 10 13....
+	for(int i=4; i<=3*n+1; i+=3){
+		cout<<i<< " ";		/	}
+
+}
