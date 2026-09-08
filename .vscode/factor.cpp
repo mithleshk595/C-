72082692bc15e830854bf3ce104f6843eaa301ac
Mithleshk595 <mithleshk0982@gmail.com>
2026-09-08 21:20:30 +0530

    Added factor.cpp

---
+++ .vscode/factor.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cout << "Enter n : ";
+	cin >> n;
+	int f = 1; // store karunga highest factor
+	
+	for(int i=1; i<=n/2; i++){
+	if(n%i==0) f = i;
+			
+		
+	}
+	cout << f;
+	
+}
