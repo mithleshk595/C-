4e250517e63ef91aedc46b6f16c290bb672ec5e8
Mithleshk595 <mithleshk0982@gmail.com>
2026-09-24 19:16:25 +0530

    Added memory.cpp

---
+++ memory.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class A {
+    public:
+        static int x;
+
+};
+ int A::x=10;
+ int main(){
+    A, a, b;
+
+    cout <<a.x <<endl;
+    cout<<b.x <<endl;
+    
+ }
